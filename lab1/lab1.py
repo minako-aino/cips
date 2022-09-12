b93f344c891e75d87711e1e420b0e6e1cf8bcd8d
nastiad <nastiadkpi@gmail.com>
2022-09-12 22:25:43 +0200

    Small changes

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -53,34 +53,45 @@ f.close()
 
 # Task2
 # according to the task ¯\_(ツ)_/¯
-w1 = 1
-w2 = 1
-w3 = 1
+w1 = [1, 0, 0, 0]
+w2 = [1, 0, 0, 0, 0]
+w3 = [1, 0, 0, 0, 0, 0, 0]
 
 
-def sum_with_coefs(array, w1=1, w2=1, w3=1):
+def sum_with_coefs(array, w1, w2, w3):
     sum = 0
-    for a in range(4):
-        sum *= w1
-        for b in range(5):
-            sum *= w2
-            for c in range(7):
-                sum *= w3
-                sum += array[a][b][c]
+    for i in range(4):
+        for j in range(5):
+            for k in range(7):
+                sum += w1[i] * w2[j] * w3[k] * array[i][j][k]
     return sum
 
 
 # case A
 a = sum_with_coefs(arrayA, w1, w2, w3)
-print("Grade A:", grades(a/140))
+print("Grade A:", a)
 
 # case B
 b = sum_with_coefs(arrayB, w1, w2, w3)
-print("Grade B:",grades(b/140))
+print("Grade B:", b)
+
 
 # case C
+def stuff(array):
+    # generate random priorities, but they sum up to 1
+    wz = np.random.dirichlet(np.ones(140), size=1)
+    a_flat = array.ravel()
+
+    sum = 0
+    for j in range(140):
+        sum += wz[0][j] * a_flat[j]
+    return sum/140
+
+
+print("Grade C (with priorities):", stuff(arrayA))
+
 c = np.sum(arrayC)/140
-print("Grade C:", round(c))
+print("Grade C (w/o priorities):", c)
 
 # Task 3
 print("331:", arrayA[2][2][0], arrayB[2][2][0], arrayC[2][2][0])
@@ -89,25 +100,13 @@ print("452:", arrayA[3][4][1], arrayB[3][4][1], arrayC[3][4][1])
 print("114:", arrayA[0][0][3], arrayB[0][0][3], arrayC[0][0][3])
 print("141:", arrayA[0][3][0], arrayB[0][3][0], arrayC[0][3][0])
 
+
 # Task 5
-stuff = []
-all_small = a[a < 0.75]
-amount = np.count_nonzero(a < 0.75)
-print(amount)
-for i in all_small:
-    arr = np.where(a == i)
-    res = [int(x.astype(int)) for x in arr]
-    stuff.append(res)
+def smol(array, N):
+    idx = array.ravel().argsort()[:N]
+    return np.stack(np.unravel_index(idx, array.shape)).T
 
-b = np.array(stuff)
-for i in range(4):
-    print(i + 1, " ", (b[:, 0] == i).sum())
-print("-----------")
 
-for i in range(5):
-    print(i + 1, " ", (b[:, 1] == i).sum())
-print("-----------")
-
-for i in range(7):
-    print(i + 1, " ", (b[:, 2] == i).sum())
-print("-----------")
+print("Smallest values:")
+for a in smol(arrayA, 5):
+    print(arrayA[a[0]][a[1]][a[2]], " - ", a)
